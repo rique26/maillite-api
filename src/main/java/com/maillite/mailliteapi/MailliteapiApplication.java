4ff39f74502350cc1cf77f3365006a76e4cb2c50
Pedro Henrique <henrique260600@gmail.com>
2026-09-11 11:08:13 -0300

    feat: commit inicial

---
+++ src/main/java/com/maillite/mailliteapi/MailliteapiApplication.java
@@ -0,0 +1,13 @@
+package com.maillite.mailliteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailliteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailliteapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maillite/mailliteapi/MailliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maillite.mailliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
